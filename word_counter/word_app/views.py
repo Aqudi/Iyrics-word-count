e07b7a47f7e13a04dde3303a3a807d2c3237724d
Aqudi <gjdigj@naver.com>
2019-05-01 12:18:17 +0900

    check import songParser test songParser

--- word_counter/word_app/views.py
+++ word_counter/word_app/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+import songParser.songParser as sp
+
 
 # Create your views here.
 def home(request):
@@ -8,5 +10,10 @@ def about(request):
     return render(request, 'about.html')
 
 def result(request):
-    data = request.GET['data']
-    return render(request, 'result.html', {"data":data})
+    text_data = request.GET['data']
+    return render(request, 'result.html', {"data":text_data})
+
+
+if __name__ == "__main__":
+    for s in sp.getData("엔플라잉 옥탑방"):
+        print(s.getText())
